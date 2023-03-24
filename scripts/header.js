76e9e616a202b256e3dad4c8793b9718a23a8730
nbalcarc <nathan.balcarcel@wsu.edu>
2023-03-23 20:33:17 -0700

    Committing changes from first milestone

--- scripts/header.js
+++ scripts/header.js
@@ -1,13 +1,18 @@
 document.write("\
     <header class=\"navigator\">\
         <ul class=\"navigator_list\">\
-          <li class=\"navigator_item\"><a href=\"contact.html\ style=\"text-decoration: none\">Contact</a></li>\
-          <li class=\"navigator_item\"><a href=\"about.html\">About</a></li>\
-          <li class=\"navigator_item\"><a href=\"catalog.html\">Reviews</a></li>\
-          <li class=\"navigator_item\"><a href=\"index.html\">Home</a></li>\
+          <li class=\"navigator_item\"><a href=\"contact.html\" style=\"text-decoration: none\">Contact</a></li>\
+          <li class=\"navigator_item\"><a href=\"about.html\" style=\"text-decoration: none\">About</a></li>\
+          <li class=\"navigator_item\"><a href=\"catalog.html\" style=\"text-decoration: none\">Reviews</a></li>\
+          <li class=\"navigator_item\"><a href=\"index.html\" style=\"text-decoration: none\">Home</a></li>\
         </ul>\
         <a class=\"logo\">Sound Advice</a>\
     </header>\
+    <br>\
+    <br>\
+    <br>\
+    <br>\
+    <br>\
     ")
 
 
