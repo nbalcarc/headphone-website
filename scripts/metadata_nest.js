d268adf5992a018c891c8486de10255a7f82db9e
nbalcarc <nathan.balcarcel@wsu.edu>
2023-03-24 18:58:09 -0700

    Added starter page for reviews

---
+++ scripts/metadata_nest.js
@@ -0,0 +1,19 @@
+document.write("\
+    <!-- Metadata -->\
+    <meta name=\"author\" content=\"Nathan Balcarcel\">\
+    <title>Sound Advice</title>\
+    <meta name=\"description\" content=\"We give sound advice for headphones, IEMs and speakers.\">\
+    <meta charset=\"utf-8\">\
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+\
+    <!-- Fonts -->\
+    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\
+    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\
+    <link href=\"https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap\" rel=\"stylesheet\">\
+\
+    <!-- CSS -->\
+    <link rel=\"stylesheet\" href=\"../css/bootstrap.css\">\
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"../css/main.css\">\
+    ")
+
+
